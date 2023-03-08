d7e0e00a80e32ab5d14e7542e961ecb96ef29d69
BxOxSxS <BxOxSxS@protonmail.com>
2023-03-08 12:29:25 +0100

    Add upload POST handler

--- pub/index.php
+++ pub/index.php
@@ -13,5 +13,15 @@ Route::add('/upload', function() {
     $twig->display("upload.html");
 });
 
+Route::add('/upload', function() {
+    global $twig;
+
+    $tempFileName = $_FILES['uploadFile']['tmp_name'];
+    $title = $_POST['title'];
+    Post::upload($tempFileName, $title);
+
+    $twig->display("index.html");
+}, 'post');
+
 Route::run('/bsadowski/pub');
 ?>
